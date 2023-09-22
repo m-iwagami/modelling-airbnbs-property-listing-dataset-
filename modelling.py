06a11ff0e2fc4c3f34584bc41d630456ff8efcbc
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-09-22 17:28:05 +0100

    Mislestone4

--- modelling.py
+++ modelling.py
@@ -6,18 +6,10 @@ from sklearn import datasets
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+
 from tabular_data import load_airbnb
-
-
-X, y = datasets.load_airbnb('clean_tabular_data.csv', "Price_Night")
-
-print(f"Number of samples in dataset: {len(X)}")
-
-def train_linear_regression_model(data_file):
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
-    X_validation, X_test, y_validation, y_test = model_selection.train_test_split(
-    X_test, y_test, test_size=0.5
-    )
 
 class LinearRegression:
     def __init__(self, n_features: int): # initalise parameters
@@ -32,5 +24,26 @@ class LinearRegression:
     def update_params(self, W, b):
         self.W = W ## set this instance's weights to the new weight value passed to the function
         self.b = b ## do the same for the bias
+
+
+def train_linear_regression_model(data_file):
+    # Load the Airbnb data with "Price_Night" as the label
+    features, labels = load_airbnb(data_file, label_column="Price_Night")
+    X, y = features, labels
+
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
+    X_validation, X_test, y_validation, y_test = model_selection.train_test_split(
+    X_test, y_test, test_size=0.5
+    )
+   
+    
+    model = LinearRegression(n_features=18)  # instantiate our linear model
+    y_pred = model(X_test)  # make predictions with data
+    print("Predictions:\n", y_pred[:10]) # print the first 10 predictions
+    
+if __name__=="__main__":
+    data_file = "clean_tabular_data.csv"
+    train_linear_regression_model(data_file)
+
 
 
--- tabular_data.py
+++ tabular_data.py
@@ -5,32 +5,31 @@ import numpy as np
 def remove_rows_with_missing_ratings(df):
     df = df.dropna(subset=['Cleanliness_rating','Accuracy_rating','Communication_rating','Location_rating','Check-in_rating','Value_rating'])
     return df
-
+    
 def combine_description_strings(df):
     df["Description"] = df["Description"].str.replace("'About this space', ", "")
     df["Description"] = df["Description"].str.replace("''", "")
+    df.dropna(subset=['Description'], inplace=True)
     return df
 
 def set_default_feature_values(df):
     """
     Replace Null value with "1".
-
-    Parameters:
-        (pd.DataFrame)
-
-    Returns:
-        (pd.DataFrame)
     """
     df["guests"].fillna(1, inplace=True)
     df["beds"].fillna(1, inplace=True)
     df["bathrooms"].fillna(1, inplace=True)
     df["bedrooms"].fillna(1, inplace=True)
+    #df["Price_Night"].fillna(0, inplace=True)
     return df
+
+
 
 def clean_tabular_data(raw_dataset):
     cleaned_dataset  = remove_rows_with_missing_ratings(raw_dataset)
     cleaned_dataset  = combine_description_strings(cleaned_dataset)
     cleaned_dataset  = set_default_feature_values(cleaned_dataset)
+    cleaned_dataset = cleaned_dataset.drop(columns = 'Unnamed: 19')
     return(cleaned_dataset)
 
 def load_airbnb(data_file, label_column):
@@ -56,8 +55,7 @@ def load_airbnb(data_file, label_column):
 
 if __name__ == "__main__":
     raw_data = pd.read_csv("listing.csv")
-    cleaned_data = clean_tabular_data(raw_data)
-    cleaned_data.to_csv('clean_tabular_data.csv', index=False)
-    cleaned_data.to_string('clean_tabular_data.txt', index=False)
+    cleaned_dataset = clean_tabular_data(raw_data)
+    cleaned_dataset.to_csv('clean_tabular_data.csv', index=False)
     
 
