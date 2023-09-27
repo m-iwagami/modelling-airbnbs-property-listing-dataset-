ae6ef937e11e5446567758903e15a2279d59c832
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-09-27 09:44:23 +0100

    Milestone4

--- modelling.py
+++ modelling.py
@@ -1,46 +1,44 @@
 
-import numpy as np
 import pandas as pd
-from sklearn.linear_model import SGDRegressor
-from sklearn import datasets
-from sklearn import model_selection
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import math
 from tabular_data import load_airbnb
-
-class LinearRegression:
-    def __init__(self, n_features: int): # initalise parameters
-        np.random.seed(10)
-        self.W = np.random.randn(n_features, 1) ## randomly initialise weight
-        self.b = np.random.randn(1) ## randomly initialise bias
-        
-    def __call__(self, X): 
-        ypred = np.dot(X, self.W) + self.b
-        return ypred # return prediction
-    
-    def update_params(self, W, b):
-        self.W = W ## set this instance's weights to the new weight value passed to the function
-        self.b = b ## do the same for the bias
 
 
 def train_linear_regression_model(data_file):
     # Load the Airbnb data with "Price_Night" as the label
-    features, labels = load_airbnb(data_file, label_column="Price_Night")
-    X, y = features, labels
+    label_column = "Price_Night"
+    features, labels = load_airbnb(data_file, label_column)
+    numerical_features = features.select_dtypes(include=['number'])
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
-    X_validation, X_test, y_validation, y_test = model_selection.train_test_split(
-    X_test, y_test, test_size=0.5
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+    numerical_features, labels, test_size=0.2, random_state=42
     )
-   
     
-    model = LinearRegression(n_features=18)  # instantiate our linear model
-    y_pred = model(X_test)  # make predictions with data
-    print("Predictions:\n", y_pred[:10]) # print the first 10 predictions
-    
+    # Create a pipeline for feature scaling and the SGDRegressor
+    eta0 = 1e-2
+    sgd_pipeline = Pipeline([("feature_scaling", StandardScaler()),
+                             ("sgd", SGDRegressor(random_state=42, eta0 = eta0))])
+    sgd_pipeline.fit(X_train, y_train)
+
+    # Make predictions on the test set
+    y_pred = sgd_pipeline.predict(X_test)
+
+    # Evaluate the model's performance on the test set
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = math.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f"RMSE: {rmse}")
+    print(f"Mean Squared Error: {mse}")
+    print(f"R-squared (R2) Score: {r2}")
+
 if __name__=="__main__":
     data_file = "clean_tabular_data.csv"
     train_linear_regression_model(data_file)
