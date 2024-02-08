e742d06441e5bbe8b5d152a777af96375ae11702
Momoko_Iwagami <iwagami.m0@gmail.com>
2024-02-08 21:12:59 +0000

    Milestone 7

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
     - A Data Science project from AiCore course
 
 ## Project Introduction
-Build a framework to systematically train, tune, and evaluate models on several tasks that are tackled by the Airbnb team
+This project aims to build a framework to systematically train, tune, and evaluate models for several tasks tackled by the Airbnb team.
 
 ## Language and Tools
-- python
-- github
+- Python
+- GitHub
 - AWS
 
 ## Project Outline
@@ -18,34 +18,36 @@ Build a framework to systematically train, tune, and evaluate models on several 
 
 
 ## Requirements
-- pip install -r requirements.txt
-- python3 
+- Install dependencies using pip install -r requirements.txt
+- Requires Python 3
 
 ## Github
-- https://github.com/m-iwagami/modelling-airbnbs-property-listing-dataset-
+- [Project Repository](https://github.com/m-iwagami/modelling-airbnbs-property-listing-dataset-)
 
 ## Modules
 
 ### Clean a dataset
 #### tabular_data.py: 
-1. remove_rows_with_missing_ratings:
-2. combine_description_strings: combines the list items into the same string. It take in the dataset as a pandas dataframe and return the same type. It removes any records with a missing description, and also remove the "About this space" prefix
-3. set_default_feature_values: replace empty values in "guests", "beds", "bathrooms", and "bedrooms" columns with "1"
-4. clean_tabular_data: takes in the raw dataframe, calls 3 functions sequentially on the output of the previous one, and returns the processed data.
-5. load_airbnb: Load Airbnb data and return features and labels
+
+1. remove_rows_with_missing_ratings: Removes rows with missing ratings.
+2. combine_description_strings: Combines list items into the same string. It takes the dataset as a pandas dataframe and returns the same type. 
+3. set_default_feature_values: Replaces empty values in the "guests," "beds," "bathrooms," and "bedrooms" columns with "1".
+4. clean_tabular_data: Takes the raw dataframe, calls three functions sequentially on the output of the previous one, and returns the processed data.
+5. load_airbnb: Loads Airbnb data and returns features and labels.
 
 ### Regression model
 #### modelling_regression.py:
 1. import_and_standarised_data:Imports the data through the load_airbnb() function and then standardises it
 
-2. split_data: Splits the data into training, validating and testing data
+2. split_data: Splits the data into training, validation, and testing data.
+
 3. train_linear_regression_model: Trains a linear regression model and evaluates its performance.
 
-4. custom_tune_regression_model_hyperparameters: Perform a grid search over hyperparameter values for a regression model.
-5. tune_regression_model_hyperparameters: Tune the hyperparameters of the Random Forest Regression model using grid search.
-6. save_model: Save the best regression model, its hyperparameters, and performance metrics to files.
-7. evaluate_all_models: Evaluate multiple regression models with different hyperparameter combinations.
-8. find_best_model: Find the best regression model among saved models.
+4. custom_tune_regression_model_hyperparameters: Performs a grid search over hyperparameter values for a regression model.
+5. tune_regression_model_hyperparameters: Tunes the hyperparameters of the Random Forest Regression model using grid search.
+6. save_model: Saves the best regression model, its hyperparameters, and performance metrics to files.
+7. evaluate_all_models: Evaluates multiple regression models with different hyperparameter combinations.
+8. find_best_model: Finds the best regression model among saved models.
 
 
 ### Classification model
@@ -75,16 +77,25 @@ To find the best classification model among a list of models, use the find_best_
 - Gradient Boosting Classifier: GardientBoostingClassifer_pram
 - Random Forest Classifier: RandomForestClassifier_pram
 
-#### main.py:
-
-#### db_creds.yaml & my_db_creds.yaml:
-
 
 ### Neural Network
+#### modelling_neural_network.py
+##### class
+- AirbnbNightlyPriceRegressionDataset class: Initializes the dataset.
+- NN(torch.nn.Module): Neural network model for regression.
+- LinearRegressionStructure(torch.nn.Module): Linear regression model with custom structure.
+- LinearRegressionModel(torch.nn.Module): Simple linear regression model 
+
+##### Methods
+- build_model_structure: Constructs the model structure based on the provided configuration.
+- train_neural_network: Trains the neural network model.
+- get_nn_config: Reads the neural network configuration from a YAML file.
+- generate_nn_config: Generates all possible configurations for neural network training.
+- find_best_nn: Finds the best neural network configuration.
+- calculate_metrics: Calculates evaluation metrics for the trained model on different datasets.
+- save_model: Saves the trained model, hyperparameters, and evaluation metrics to files.
+- load_data:     Load the Airbnb property listing data from 'clean_tabular_data.csv'. Target variable 'Price_Night'.
+- load_data_2: Load the Airbnb property listing data from 'clean_tabular_data.csv'. Returns Target variable 'bedrooms'.
 
 
-## Libraries
 
-
-## Furthur work
-This project involves the implementation and evaluation of classification models for predicting categories of Airbnb property listings. The code is organized into a class, `ClassificationModel`, which encapsulates various methods for data processing, model evaluation, hyperparameter tuning, and model saving.
--- modelling_neural_network.py
+++ modelling_neural_network.py
@@ -13,7 +13,6 @@ import torch.optim as optim
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import yaml
@@ -23,6 +22,13 @@ import yaml
 
 class AirbnbNightlyPriceRegressionDataset(Dataset):
     def __init__(self, X, y):
+        """
+        Initializes the dataset.
+
+        Args:
+            X (pandas.DataFrame): Features.
+            y (pandas.Series): Target variable.
+        """
         super().__init__()
         assert len(X) == len(y)
         self.X = X
@@ -36,7 +42,17 @@ class AirbnbNightlyPriceRegressionDataset(Dataset):
 
             
 class NN(torch.nn.Module):
+    """
+    Neural network model for regression.
+
+    This class defines a simple neural network architecture for regression tasks.
+
+    Attributes:
+        layers (torch.nn.Sequential): Layers of the neural network.
+
+    """
     def __init__(self):
+        
         super().__init__()
         self.layers = torch.nn.Sequential(
             torch.nn.Linear(11,16),
@@ -50,8 +66,24 @@ class NN(torch.nn.Module):
         return self.layers(X)
 
 
-class LinearRegressionStructure(torch.nn.Module):#
+class LinearRegressionStructure(torch.nn.Module):
+    """
+    Linear regression model with custom structure.
+
+    This class initializes a linear regression model with a custom structure defined by the user.
+
+    Attributes:
+        layers (torch.nn.Sequential): Layers of the linear regression model.
+
+    """
     def __init__(self, config_model_structure):
+        """
+        Initializes the linear regression model with custom structure.
+
+        Args:
+            config_model_structure (OrderedDict): Configuration of the model structure.
+        """
+        
         super().__init__()
         self.layers = nn.Sequential(
         config_model_structure
@@ -63,6 +95,15 @@ class LinearRegressionStructure(torch.nn.Module):#
 
 
 class LinearRegressionModel(torch.nn.Module):
+    """
+    Simple linear regression model.
+
+    This class initializes a simple linear regression model.
+
+    Attributes:
+        linear_layer (torch.nn.Linear): Linear layer of the regression model.
+    """
+
     def __init__(self, input_size):
         super().__init__()
         self.linear_layer = torch.nn.Linear(input_size, 1)
@@ -70,10 +111,17 @@ class LinearRegressionModel(torch.nn.Module):
     def forward(self, features):
         return self.linear_layer(features)
 
-    
+def build_model_structure(config):
+    """
+    build_model_structure
+    Constructs the model structure based on the provided configuration.
 
-def get_model_structure(config):
+    Args:
+        config (dict): Configuration of the model structure.
 
+    Returns:
+        OrderedDict: Model structure configuration.
+    """
     hidden_layer_size = config['hidden_layer_width']
     linear_depth = config['depth']
 
@@ -96,10 +144,27 @@ def get_model_structure(config):
     #output layer
     config_dict['output'] = nn.Linear(11, 1)
     return config_dict
-def train(model, train_dataloader, val_dataloader, nn_config, epochs=16):
 
+
+def train_neural_network(model, train_dataloader, val_dataloader, nn_config, epochs=40):
+    
+    """
+    Trains the neural network model.
+
+    Args:
+        model (torch.nn.Module): Neural network model to be trained.
+        train_dataloader (DataLoader): DataLoader for the training dataset.
+        val_dataloader (DataLoader): DataLoader for the validation dataset.
+        nn_config (dict): Configuration for the neural network training.
+        epochs (int, optional): Number of epochs. Defaults to 16.
+
+    Returns:
+        tuple: A tuple containing the trained model, training duration, interference latency, and timestamp.
+    """
+    
     lr = nn_config['lr']
     
+    # Select optimizer based on the provided configuration
     if nn_config['optimiser'] == "SGD":
         optimizer = torch.optim.SGD(model.parameters(), lr)
     elif nn_config['optimiser'] == "Adam":
@@ -108,25 +173,26 @@ def train(model, train_dataloader, val_dataloader, nn_config, epochs=16):
         optimizer = torch.optim.SparseAdam(model.parameters(), lr)
 
 
-    criterion = nn.MSELoss()
-    writer = SummaryWriter()
+    criterion = nn.MSELoss() # Define the loss function
+    writer = SummaryWriter() # Tensorboard writer for logging
     batch_idx = 0
     _batch_idx = 0
     
-    starting_time = datetime.now()
+    starting_time = datetime.now() # Start time for training duration calculation
     for epoch in range(epochs):
         for batch in train_dataloader:
             features, labels = batch
-            optimizer.zero_grad()
+            optimizer.zero_grad() # Reset gradients
             predictions = model(features)
             loss = criterion(predictions, labels)
-            loss.backward()
+            loss.backward() # Backpropagation
             optimizer.step()
             writer.add_scalar(('loss'), loss.item(), batch_idx)
             batch_idx += 1
             #print(f"Training Loss: {loss.item()}")
 
         prediction_time_list = []
+        # Validation phase
         for _batch in val_dataloader:
             features, labels =  _batch
             optimizer.zero_grad()  # Reset gradients before inference
@@ -141,31 +207,48 @@ def train(model, train_dataloader, val_dataloader, nn_config, epochs=16):
 
     #End of the training_duratioin time 
     ending_time = datetime.now()
-            
-    # getting the timestamp
+    
+    # Calculate training duration
     training_duration =  ending_time - starting_time
+    
+    # Generate timestamp
     time_filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    
+    # Calculate average interference latency
     interference_latency =  sum(prediction_time_list) / len(prediction_time_list)
 
-    print(model)
-    #return model, training_duration, interference_latency, time_filename
+    return model, training_duration, interference_latency, time_filename
 
 def get_nn_config():
+    """
+    Reads the neural network configuration from a YAML file.
+
+    Returns:
+        dict: Neural network configuration.
+    """
     with open('nn_config.yaml', 'r') as file:
         hyperparameter = yaml.safe_load(file)
     return hyperparameter
 
 def generate_nn_config():
+    """
+    Generates all possible configurations for neural network training.
+
+    Returns:
+        list: List of dictionaries containing different configurations.
+    """
     
+    # Define dictionaries for different hyperparameters
     combined_dictionary = {
-    'optimiser': ['SGD', 'Adam', 'SparseAdam'],
-    'lr': [0.01, 0.001, 0.0001],
+    'optimiser': ['SGD', 'Adam', 'RMSProp'],
+    'lr': [0.0005, 0.0008, 0.00001],
     'hidden_layer_width': [16, 11, 10],
-    'depth': [5, 3,1]
+    'depth': [5, 3, 1]
     }
 
     config_dict_list = []
-    
+
+    # Generate all combinations of hyperparameters
     for iteration in itertools.product(*combined_dictionary.values()):
         config_dict = {
             'optimiser': iteration[0],
@@ -176,19 +259,34 @@ def generate_nn_config():
 
     return config_dict_list
     
-    
 
-def find_best_nn(config_dict_list, train_dataloader, validation_dataloader, test_dataloader, model):
-    
-    # For each configuration, redefine the nn_model and the training function
+def find_best_nn(config_dict_list, train_dataloader, validation_dataloader, test_dataloader):
+    """
+    Finds the best neural network configuration.
+
+    Args:
+        config_dict_list (list): List of dictionaries containing different configurations.
+        train_dataloader (DataLoader): DataLoader for the training dataset.
+        validation_dataloader (DataLoader): DataLoader for the validation dataset.
+        test_dataloader (DataLoader): DataLoader for the test dataset.
+
+    Returns:
+        tuple: A tuple containing the best metrics and hyperparameters.
+    """    
+    # Iterate over each configuration in the list
     for i, nn_config in enumerate(config_dict_list):
+        
+        # Build model structure based on the current configuration
+        model_structure = build_model_structure(nn_config)
+        
+        # Create a model using the model structure
+        model = LinearRegressionStructure(model_structure)
+        
+        # Train the neural network with the current configuration
+        best_model, training_duration, inference_latency, time_stamp = train_neural_network(model, train_dataloader, val_dataloader, nn_config)
 
         best_metrics_ = None
         best_hyperparameters = nn_config
-
-
-        # Train the NN model using the model, the dataloaders and nn_config file
-        best_model, training_duration, inference_latency, time_stamp = train(model, train_dataloader, validation_dataloader, nn_config)
 
         # Calculate the metrics
         train_RMSE_loss, validation_RMSE_loss, test_RMSE_loss, train_R_squared, validation_R_squared, test_R_squared = calculate_metrics(best_model, train_dataloader, validation_dataloader, test_dataloader)
@@ -208,20 +306,28 @@ def find_best_nn(config_dict_list, train_dataloader, validation_dataloader, test
 
         if i >= 20:
             break
-        
-        
-
+      
+    # Save the best model, hyperparameters, and metrics  
     save_model(best_model_, best_hyperparameters_, best_metrics_, time_stamp)
-
     
-
     return best_metrics_, best_hyperparameters_
         
-    
-
+        
 def calculate_metrics(best_model, train_loader, validation_loader, test_loader):
+    """
+    Calculates evaluation metrics for the trained model on different datasets.
+
+    Args:
+        best_model (torch.nn.Module): Trained neural network model.
+        train_loader (DataLoader): DataLoader for the training dataset.
+        validation_loader (DataLoader): DataLoader for the validation dataset.
+        test_loader (DataLoader): DataLoader for the test dataset.
+
+    Returns:
+        tuple: A tuple containing the calculated RMSE loss and R-squared values for training, validation, and test datasets.
+    """
     def calculate_metrics_for_loader(loader):
-        y = np.array([]) # Targets
+        y = np.array([]) 
         y_hat = np.array([])
 
         for features, labels in loader:
@@ -232,32 +338,40 @@ def calculate_metrics(best_model, train_loader, validation_loader, test_loader):
             y_hat = np.concatenate((y_hat, prediction.detach().numpy()))
             # If the predictions include nan values, assign poor metrics to discard the model later
             if np.isnan(y_hat).any():
-                RMSE_loss = 1000000
+                RMSE_loss = 1000
                 R_squared = 0
-            else:  # Else, calculate RMSE and R^2
+            else:  # Calculate RMSE and R^2
                 RMSE_loss = mean_squared_error(y, y_hat, squared=False)
                 R_squared = r2_score(y, y_hat)
 
         return RMSE_loss, R_squared
-
+    
+    # Calculate metrics for each loader (train, validation, test)
     train_RMSE_loss, train_R_squared = calculate_metrics_for_loader(train_loader)
     validation_RMSE_loss, validation_R_squared = calculate_metrics_for_loader(validation_loader)
     test_RMSE_loss, test_R_squared = calculate_metrics_for_loader(test_loader)
 
-    return train_RMSE_loss, validation_RMSE_loss, test_RMSE_loss, train_R_squared, validation_R_squared, test_R_squared
-
-    #print(train_RMSE_loss, validation_RMSE_loss, test_RMSE_loss, train_R_squared, validation_R_squared, test_R_squared)
-    
+    return train_RMSE_loss, validation_RMSE_loss, test_RMSE_loss, train_R_squared, validation_R_squared, test_R_squared    
 
 def save_model(model, best_hyperparameters_, best_metrics, time_stamp):
+    """
+    Saves the trained model, hyperparameters, and evaluation metrics to files.
+
+    Args:
+        model (torch.nn.Module): Trained neural network model.
+        best_hyperparameters_ (dict): Best hyperparameters configuration.
+        best_metrics (dict): Best evaluation metrics.
+        time_stamp (str): Timestamp used for creating directories.
+    """
+    # Destination directory for saving model files
     dest = '/Users/momo/aicore/github/modelling-airbnbs-property-listing-dataset-/neural_networks/regression'
     new_path = f"{dest}/{time_stamp}"
     os.mkdir(new_path)
     
-    #Save model
+    # Save model state dictionary to a file
     torch.save(model.state_dict(), f'{new_path}/model.pt')
     
-    #Save Hyperparameter
+    # Save hyperparameters to a JSON file
     hyperparameter = {
         'optimiser': best_hyperparameters_['optimiser'],
         'lr': best_hyperparameters_['lr'],
@@ -291,26 +405,45 @@ def save_model(model, best_hyperparameters_, best_metrics, time_stamp):
     return
         
 def load_data():
+    """
+    Load the Airbnb property listing data from 'clean_tabular_data.csv'.
+
+    Returns:
+    X (DataFrame): Features matrix containing all columns except 'Price_Night'.
+    y (Series): Target variable 'Price_Night'.
+    """
     dataframe = pd.read_csv('clean_tabular_data.csv')
     X, y = dataframe.drop('Price_Night', axis=1), dataframe['Price_Night']
     return X,y
 
+def load_data_2():
+    """
+    Load the Airbnb property listing data from 'clean_tabular_data.csv'.
 
+    Returns:
+    X (DataFrame): Features matrix containing all columns except 'bedrooms'.
+    y (Series): Target variable 'bedrooms'.
+    """
+    dataframe = pd.read_csv('clean_tabular_data.csv')
+    X, y = dataframe.drop('bedrooms', axis=1), dataframe['bedrooms']
+    return X,y
+
+# Load data
 X,y = load_data()
 dataset = AirbnbNightlyPriceRegressionDataset(X, y)
 
 
-# Split tarining, validation, testing samples (80,10,10)
+# Split data into training, validation, and testing samples (80%, 10%, 10%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5)
 
-#Dataset
+# Define datasets
 train_dataset = AirbnbNightlyPriceRegressionDataset(X_train, y_train)
 val_dataset = AirbnbNightlyPriceRegressionDataset(X_validation, y_validation)
 test_dataset = AirbnbNightlyPriceRegressionDataset(X_test, y_test)
 
 
-#DataLoader
+# Define dataloaders
 batch_size = 16 
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -321,15 +454,5 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 if __name__ == "__main__":
     # Generate configurations
     config_dict_list = generate_nn_config()
-    
-    # Loop over each configuration
-    for config in config_dict_list:
-        #model = NN()
-        # Get model structure based on the current configuration
-        model_structure = get_model_structure(config)
-        # Create LinearRegression_jared model using the model structure
-        model = LinearRegressionStructure(model_structure)
-        train(model, train_dataloader, val_dataloader, config)
-        
-    model, training_duration, interference_latency, time_filename = train(model, train_dataloader, val_dataloader, config)
-    find_best_nn(config_dict_list, train_dataloader, val_dataloader, test_dataloader, model)
+    # Find the best neural network configuration
+    find_best_nn(config_dict_list, train_dataloader, val_dataloader, test_dataloader)
